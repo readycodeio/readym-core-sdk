052fecc9c20f2995fb8699dc33ade8203e62226b
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2025-05-28 19:47:08 +0200

    Support generating cached events

--- src/ReadyM.Api.Multiplayer/RpcEventAttribute.cs
+++ src/ReadyM.Api.Multiplayer/RpcEventAttribute.cs
@@ -4,4 +4,4 @@ using ReadyM.Relay.Common.Protocol.Enums;
 namespace ReadyM.Api.Multiplayer;
 
 [AttributeUsage(AttributeTargets.Method)]
-public sealed class RpcEventAttribute(RelayMode relayMode) : Attribute;
+public sealed class RpcEventAttribute(RelayMode relayMode, EventCaching caching = EventCaching.DoNotCache) : Attribute;
