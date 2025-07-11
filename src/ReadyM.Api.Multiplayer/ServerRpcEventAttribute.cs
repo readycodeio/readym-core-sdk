628ef72618c6ed06098d5baec5e3b4c99510f829
Kamil Opalach <kamil.opalach@readycode.io>
2025-07-11 16:06:13 +0200

    Add generators

---
+++ src/ReadyM.Api.Multiplayer/ServerRpcEventAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using ReadyM.Relay.Common.Protocol.Enums;
+
+namespace ReadyM.Api.Multiplayer;
+
+[AttributeUsage(AttributeTargets.Method)]
+#pragma warning disable CS9113 // Parameter is unread.
+public sealed class ServerRpcEventAttribute(ServerRpcCode rpcCode) : Attribute;
+public sealed class ServerRpcHandlerAttribute(ServerRpcCode rpcCode) : Attribute;
+#pragma warning restore CS9113 // Parameter is unread.
