dd3f63145b6fa8c7e5491f59b8f8f483dc7db987
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2025-07-24 13:03:01 +0200

    Fix PR issues

--- src/ReadyM.Api.Multiplayer/ServerRpcEventAttribute.cs
+++ src/ReadyM.Api.Multiplayer/ServerRpcEventAttribute.cs
@@ -2,8 +2,10 @@
 
 namespace ReadyM.Api.Multiplayer;
 
-[AttributeUsage(AttributeTargets.Method)]
 #pragma warning disable CS9113 // Parameter is unread.
+[AttributeUsage(AttributeTargets.Method)]
 public sealed class ServerRpcEventAttribute(string name) : Attribute;
+
+[AttributeUsage(AttributeTargets.Method)]
 public sealed class ServerRpcHandlerAttribute(string name) : Attribute;
 #pragma warning restore CS9113 // Parameter is unread.
