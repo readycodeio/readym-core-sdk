0b433f302f5056f795a423b8a5929ef5e5412d72
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2025-07-24 15:33:32 +0200

    Set tick rate to 15 ms

--- src/ReadyM.Api.Multiplayer/NetworkedEntityManager.cs
+++ src/ReadyM.Api.Multiplayer/NetworkedEntityManager.cs
@@ -61,11 +61,6 @@ public sealed class NetworkedEntityManager : IDisposable
         return _store.CreateEntity(archetypeId, b => b.Add(netId));
     }
 
-    // internal Entity CreateRemoteNetworkedEntity(NetworkIdComponent netId)
-    // {
-    //     return _store.CreateEntity(b => b.Add(netId));
-    // }
-
     public bool TryGetEntityByNetworkId(NetworkIdComponent netId, [NotNullWhen(true)] out Entity? entity)
     {
         var ix = _store.ComponentIndex<NetworkIdComponent, NetworkIdComponent>();
