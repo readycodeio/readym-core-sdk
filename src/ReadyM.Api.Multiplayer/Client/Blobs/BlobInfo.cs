34a6228f4acb231fe0f8c40d328a9f10dcfa88b7
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-02-17 14:37:02 +0100

    Remove old blob API

--- src/ReadyM.Api.Multiplayer/Client/Blobs/BlobInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ReadyM.Api.Multiplayer.Client.Blobs;
-
-public class BlobInfo(string name, byte[] content)
-{
-    public string Name { get; } = name;
-    public byte[] Content { get; } = content;
-}
