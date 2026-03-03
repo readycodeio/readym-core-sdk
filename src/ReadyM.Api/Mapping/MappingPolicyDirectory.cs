a0ebe5f21b51a799853f5bca63ea8e0f54605139
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-03-03 16:19:04 +0100

    Thread-safe policy directory

--- src/ReadyM.Api/Mapping/MappingPolicyDirectory.cs
+++ src/ReadyM.Api/Mapping/MappingPolicyDirectory.cs
@@ -11,126 +11,137 @@ namespace ReadyM.Api.Mapping;
 
 public class MappingPolicyDirectory(DataSideChannel sideChannel) : IMappingPolicyDirectory
 {
+    private readonly object _createDeleteLock = new();
     private readonly Dictionary<(ArchetypeId, Type), IMappingCreateDeletePolicyBase> _createDeletePolicies = new();
     private readonly Dictionary<ArchetypeId, List<IMappingCreateDeletePolicyFactory>> _archetypeCreateDeletePolicyFactories = new();
     private readonly List<IMappingCreateDeletePolicyFactory> _createDeletePolicyFactories = new();
-    
+
+    private readonly object _dataLock = new();
     private readonly Dictionary<(ArchetypeId, Type, Type), IMappingDataPolicyBase> _dataPolicies = new();
     private readonly Dictionary<ArchetypeId, List<IMappingDataPolicyFactory>> _archetypeDataPolicyFactories = new();
     private readonly List<IMappingDataPolicyFactory> _dataPolicyFactories = new();
 
+    private readonly object _eventLock = new();
     private readonly Dictionary<(Type, Type), IMappingEventPolicyBase> _eventPolicies = new();
     private readonly List<IMappingEventPolicyFactory> _eventPolicyFactories = new();
 
     public IMappingCreateDeletePolicy<TGameObject> ForCreateDelete<TGameObject>(ArchetypeId archetypeId)
         where TGameObject : class
     {
-        var key = (archetypeId, typeof(TGameObject));
-        
-        if (!_createDeletePolicies.TryGetValue(key, out var untypedPolicy))
+        lock (_createDeleteLock)
         {
-            if (_archetypeCreateDeletePolicyFactories.TryGetValue(archetypeId, out var factories))
+            var key = (archetypeId, typeof(TGameObject));
+            if (!_createDeletePolicies.TryGetValue(key, out var untypedPolicy))
             {
-                foreach (var factory in factories)
+                if (_archetypeCreateDeletePolicyFactories.TryGetValue(archetypeId, out var factories))
                 {
-                    if (!factory.Supports(typeof(TGameObject)))
-                        continue;
-                
-                    untypedPolicy = factory.CreatePolicy(archetypeId, typeof(TGameObject));
-                    break;
+                    foreach (var factory in factories)
+                    {
+                        if (!factory.Supports(typeof(TGameObject)))
+                            continue;
+
+                        untypedPolicy = factory.CreatePolicy(archetypeId, typeof(TGameObject));
+                        break;
+                    }
                 }
+
+                if (untypedPolicy == null)
+                {
+                    foreach (var factory in _createDeletePolicyFactories)
+                    {
+                        if (!factory.Supports(typeof(TGameObject)))
+                            continue;
+
+                        untypedPolicy = factory.CreatePolicy(archetypeId, typeof(TGameObject));
+                        break;
+                    }
+                }
+
+                if (untypedPolicy == null)
+                    throw new ArgumentException($"No create/delete policy registered for archetype {archetypeId} and game object type {typeof(TGameObject)}");
+
+                _createDeletePolicies.Add(key, untypedPolicy);
             }
 
-            if (untypedPolicy == null)
-            {
-                foreach (var factory in _createDeletePolicyFactories)
-                {
-                    if (!factory.Supports(typeof(TGameObject)))
-                        continue;
-
-                    untypedPolicy = factory.CreatePolicy(archetypeId, typeof(TGameObject));
-                    break;
-                }
-            }
-
-            if (untypedPolicy == null)
-                throw new ArgumentException($"No create/delete policy registered for archetype {archetypeId} and game object type {typeof(TGameObject)}");
-
-            _createDeletePolicies.Add(key, untypedPolicy);
+            return (IMappingCreateDeletePolicy<TGameObject>)untypedPolicy;
         }
-
-        return (IMappingCreateDeletePolicy<TGameObject>)untypedPolicy;
     }
 
     public IMappingDataPolicy<TContext> ForData<TData, TContext>(ArchetypeId archetypeId)
         where TData : struct, IMappingContext<TContext>
         where TContext : struct
     {
-        var key = (archetypeId, typeof(TData), typeof(TContext));
-        
-        if (!_dataPolicies.TryGetValue(key, out var untypedPolicy))
+        lock (_dataLock)
         {
-            if (_archetypeDataPolicyFactories.TryGetValue(archetypeId, out var factories))
+            var key = (archetypeId, typeof(TData), typeof(TContext));
+
+            if (!_dataPolicies.TryGetValue(key, out var untypedPolicy))
             {
-                foreach (var factory in factories)
+                if (_archetypeDataPolicyFactories.TryGetValue(archetypeId, out var factories))
                 {
-                    if (!factory.Supports(typeof(TData), typeof(TContext)))
-                        continue;
-                
-                    untypedPolicy = factory.CreatePolicy<TContext>(archetypeId, typeof(TData));
-                    break;
+                    foreach (var factory in factories)
+                    {
+                        if (!factory.Supports(typeof(TData), typeof(TContext)))
+                            continue;
+
+                        untypedPolicy = factory.CreatePolicy<TContext>(archetypeId, typeof(TData));
+                        break;
+                    }
                 }
+
+                if (untypedPolicy == null)
+                {
+                    foreach (var factory in _dataPolicyFactories)
+                    {
+                        if (!factory.Supports(typeof(TData), typeof(TContext)))
+                            continue;
+
+                        untypedPolicy = factory.CreatePolicy<TContext>(archetypeId, typeof(TData));
+                        break;
+                    }
+                }
+
+                if (untypedPolicy == null)
+                    throw new ArgumentException($"No data policy registered for archetype {archetypeId} and data type {typeof(TData)}");
+
+                _dataPolicies.Add(key, untypedPolicy);
             }
 
-            if (untypedPolicy == null)
-            {
-                foreach (var factory in _dataPolicyFactories)
-                {
-                    if (!factory.Supports(typeof(TData), typeof(TContext)))
-                        continue;
-                
-                    untypedPolicy = factory.CreatePolicy<TContext>(archetypeId, typeof(TData));
-                    break;
-                }
-            }
-            
-            if (untypedPolicy == null)
-                throw new ArgumentException($"No data policy registered for archetype {archetypeId} and data type {typeof(TData)}");
-            
-            _dataPolicies.Add(key, untypedPolicy);
+            return (IMappingDataPolicy<TContext>)untypedPolicy;
         }
-        
-        return (IMappingDataPolicy<TContext>)untypedPolicy;
     }
-    
+
     public IMappingDataPolicy<Entity> ForData<TData>(ArchetypeId archetypeId)
         where TData : struct, IMappingContext<Entity>
         => ForData<TData, Entity>(archetypeId);
 
     public IMappingEventPolicy<TContext> ForEvent<TEvent, TContext>()
-        where TEvent : struct, IMappingContext<TContext> 
+        where TEvent : struct, IMappingContext<TContext>
         where TContext : struct
     {
-        var key = (typeof(TEvent), typeof(TContext));
-
-        if (!_eventPolicies.TryGetValue(key, out var untypedPolicy))
+        lock (_eventLock)
         {
-            foreach (var factory in _eventPolicyFactories)
-            {
-                if (!factory.Supports(typeof(TEvent), typeof(TContext)))
-                    continue;
-                
-                untypedPolicy = factory.CreatePolicy<TContext>(typeof(TEvent));
-                break;
-            }
-            
-            if (untypedPolicy == null)
-                throw new ArgumentException($"No event policy registered for event type {typeof(TEvent)}");
-            
-            _eventPolicies.Add(key, untypedPolicy);
-        }
+            var key = (typeof(TEvent), typeof(TContext));
 
-        return (IMappingEventPolicy<TContext>)untypedPolicy;
+            if (!_eventPolicies.TryGetValue(key, out var untypedPolicy))
+            {
+                foreach (var factory in _eventPolicyFactories)
+                {
+                    if (!factory.Supports(typeof(TEvent), typeof(TContext)))
+                        continue;
+
+                    untypedPolicy = factory.CreatePolicy<TContext>(typeof(TEvent));
+                    break;
+                }
+
+                if (untypedPolicy == null)
+                    throw new ArgumentException($"No event policy registered for event type {typeof(TEvent)}");
+
+                _eventPolicies.Add(key, untypedPolicy);
+            }
+
+            return (IMappingEventPolicy<TContext>)untypedPolicy;
+        }
     }
 
     public IMappingEventPolicy<Entity> ForEvent<TEvent>()
@@ -143,7 +154,7 @@ public class MappingPolicyDirectory(DataSideChannel sideChannel) : IMappingPolic
     {
         _createDeletePolicyFactories.Add(factory);
     }
-    
+
     public void RegisterDefaultCreateDelete(ArchetypeId archetypeId, IMappingCreateDeletePolicyFactory factory)
     {
         if (!_archetypeCreateDeletePolicyFactories.TryGetValue(archetypeId, out var factories))
@@ -151,6 +162,7 @@ public class MappingPolicyDirectory(DataSideChannel sideChannel) : IMappingPolic
             factories = new List<IMappingCreateDeletePolicyFactory>();
             _archetypeCreateDeletePolicyFactories.Add(archetypeId, factories);
         }
+
         factories.Add(factory);
     }
 
@@ -169,7 +181,7 @@ public class MappingPolicyDirectory(DataSideChannel sideChannel) : IMappingPolic
     {
         _createDeletePolicies.Add((archetypeId, typeof(TGameObject)), policy);
     }
-    
+
     public void RegisterCreateDelete<TGameObject>(
         ArchetypeId archetypeId,
         Func<TGameObject, bool> shouldCreatePropagate,
@@ -179,14 +191,14 @@ public class MappingPolicyDirectory(DataSideChannel sideChannel) : IMappingPolic
         var policy = new FuncCreateDeletePolicy<TGameObject>(shouldCreatePropagate, shouldDeletePropagate);
         RegisterCreateDelete(archetypeId, policy);
     }
-    
+
     // ---
-    
+
     public void RegisterDefaultData(IMappingDataPolicyFactory factory)
     {
         _dataPolicyFactories.Add(factory);
     }
-    
+
     public void RegisterDefaultData<TContext>(
         Func<TContext, bool> shouldEcsCopyToGame,
         Func<TContext, bool> shouldGameCopyToEcs,
@@ -195,7 +207,7 @@ public class MappingPolicyDirectory(DataSideChannel sideChannel) : IMappingPolic
         var factory = new FuncDataPolicyFactory<TContext>(shouldEcsCopyToGame, shouldGameCopyToEcs, shouldSetLocally);
         RegisterDefaultData(factory);
     }
-    
+
     public void RegisterDefaultData(ArchetypeId archetypeId, IMappingDataPolicyFactory factory)
     {
         if (!_archetypeDataPolicyFactories.TryGetValue(archetypeId, out var factories))
@@ -203,9 +215,10 @@ public class MappingPolicyDirectory(DataSideChannel sideChannel) : IMappingPolic
             factories = new List<IMappingDataPolicyFactory>();
             _archetypeDataPolicyFactories.Add(archetypeId, factories);
         }
+
         factories.Add(factory);
     }
-    
+
     public void RegisterDefaultData<TContext>(
         ArchetypeId archetypeId,
         Func<TContext, bool> shouldEcsCopyToGame,
@@ -215,7 +228,7 @@ public class MappingPolicyDirectory(DataSideChannel sideChannel) : IMappingPolic
         var factory = new FuncDataPolicyFactory<TContext>(shouldEcsCopyToGame, shouldGameCopyToEcs, shouldSetLocally);
         RegisterDefaultData(archetypeId, factory);
     }
-    
+
     public void RegisterData<TData, TContext>(ArchetypeId archetypeId, IMappingDataPolicy<TContext> policy)
         where TData : struct
         where TContext : struct
@@ -236,14 +249,14 @@ public class MappingPolicyDirectory(DataSideChannel sideChannel) : IMappingPolic
             shouldRunLocally);
         RegisterData<TData, Entity>(archetypeId, policy);
     }
-    
+
     // ---
-    
+
     public void RegisterDefaultEvent(IMappingEventPolicyFactory factory)
     {
         _eventPolicyFactories.Add(factory);
     }
-    
+
     public void RegisterDefaultEvent<TContext>(
         Func<TContext, bool> shouldGameEventPropagate,
         Func<TContext, bool> shouldEcsEventPropagate,
@@ -255,14 +268,14 @@ public class MappingPolicyDirectory(DataSideChannel sideChannel) : IMappingPolic
             shouldRunLocally);
         RegisterDefaultEvent(policyFactory);
     }
-    
+
     public void RegisterEvent<TEvent, TContext>(IMappingEventPolicy<TContext> policy)
         where TEvent : struct, IEquatable<TEvent>
         where TContext : struct
     {
         _eventPolicies.Add((typeof(TEvent), typeof(TContext)), policy);
     }
-    
+
     public void RegisterEvent<TEvent, TContext>(
         ShouldPropagateToEcsDelegate<TContext> shouldPropagateToEcs,
         ShouldPropagateToGameDelegate<TContext> shouldPropagateToGame,
@@ -273,7 +286,7 @@ public class MappingPolicyDirectory(DataSideChannel sideChannel) : IMappingPolic
         var policy = new FuncEventPolicy<TEvent, TContext>(shouldPropagateToEcs, shouldPropagateToGame, shouldRunLocally, sideChannel);
         RegisterEvent<TEvent, TContext>(policy);
     }
-    
+
     public void RegisterEvent<TEvent>(
         ShouldPropagateToEcsDelegate<Entity> shouldPropagateToEcs,
         ShouldPropagateToGameDelegate<Entity> shouldPropagateToGame,
