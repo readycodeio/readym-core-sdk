1ffbee682db11411317b20361e4c759b5fecd87f
Juliusz Kopczewski <julek.kopczewski@readycode.io>
2026-04-13 05:17:36 +0200

    Diagnostics tests added

---
+++ src/ReadyM.Api.Generators/GeneratorField.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace ReadyM.Api.Generators;
+
+internal class GeneratorField(
+    string name,
+    ITypeSymbol type,
+    int order,
+    bool readOnly,
+    bool isInvalid)
+{
+    public string Name { get; } = name;
+    public ITypeSymbol Type { get; } = type;
+    public int Order { get; } = order;
+    public bool ReadOnly { get; } = readOnly;
+    public bool IsInvalid { get; } = isInvalid;
+}
