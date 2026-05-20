4186e04e98c5bf98ac9a6a391e4bdb62fac58faf
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-05-20 17:32:53 +0200

    Non-generic registration methods

--- src/ReadyM.Api.Multiplayer/Mapping/Events/EventQueue.cs
+++ src/ReadyM.Api.Multiplayer/Mapping/Events/EventQueue.cs
@@ -8,18 +8,30 @@ internal class EventQueue(ILogger logger)
 {
     private abstract class EntryBase
     {
-        // empty
+        public abstract void Invoke(in object ev);
     }
 
     private abstract class EntryBase<TEvent> : EntryBase
     {
+        public override void Invoke(in object ev)
+        {
+            if (ev is TEvent typedEv)
+            {
+                Invoke(typedEv);
+            }
+            else
+            {
+                throw new InvalidOperationException($"Invalid event type passed to entry. Expected {typeof(TEvent).FullName}, got {ev.GetType().FullName}");
+            }
+        }
+
         public abstract void Invoke(in TEvent ev);
     }
 
     private class Entry<TEvent, TArg>(ILogger logger) : EntryBase<TEvent>
     {
         private readonly List<(Action<TEvent, TArg>, TArg)> _handlers = new();
-        
+
         public void RegisterHandler(Action<TEvent, TArg> handler, TArg arg)
         {
             _handlers.Add((handler, arg));
@@ -31,7 +43,7 @@ internal class EventQueue(ILogger logger)
             {
                 logger.LogWarning("Invoking event of type {EventType} with no handlers registered", typeof(TEvent).FullName);
             }
-            
+
             foreach (var (handler, arg) in _handlers)
             {
                 handler(ev, arg);
@@ -39,10 +51,33 @@ internal class EventQueue(ILogger logger)
         }
     }
     
+    private class OpaqueEntry<TArg>(ILogger logger) : EntryBase
+    {
+        private readonly List<(Action<object, TArg>, TArg)> _handlers = new();
+
+        public void RegisterHandler(Action<object, TArg> handler, TArg arg)
+        {
+            _handlers.Add((handler, arg));
+        }
+
+        public override void Invoke(in object ev)
+        {
+            if (_handlers.Count == 0)
+            {
+                logger.LogWarning("Invoking event of type {EventType} with no handlers registered", ev.GetType().FullName);
+            }
+
+            foreach (var (handler, arg) in _handlers)
+            {
+                handler(ev, arg);
+            }
+        }
+    }
+
     private class Entry<TEvent, TArg0, TArg1>(ILogger logger) : EntryBase<TEvent>
     {
         private readonly List<(Action<TEvent, TArg0, TArg1>, TArg0, TArg1)> _handlers = new();
-        
+
         public void RegisterHandler(Action<TEvent, TArg0, TArg1> handler, TArg0 arg0, TArg1 arg1)
         {
             _handlers.Add((handler, arg0, arg1));
@@ -54,18 +89,18 @@ internal class EventQueue(ILogger logger)
             {
                 logger.LogWarning("Invoking event of type {EventType} with no handlers registered", typeof(TEvent).FullName);
             }
-            
+
             foreach (var (handler, arg0, arg1) in _handlers)
             {
                 handler(ev, arg0, arg1);
             }
         }
     }
-    
+
     private class Entry<TEvent, TArg0, TArg1, TArg2>(ILogger logger) : EntryBase<TEvent>
     {
         private readonly List<(Action<TEvent, TArg0, TArg1, TArg2>, TArg0, TArg1, TArg2)> _handlers = new();
-        
+
         public void RegisterHandler(Action<TEvent, TArg0, TArg1, TArg2> handler, TArg0 arg0, TArg1 arg1, TArg2 arg2)
         {
             _handlers.Add((handler, arg0, arg1, arg2));
@@ -77,7 +112,7 @@ internal class EventQueue(ILogger logger)
             {
                 logger.LogWarning("Invoking event of type {EventType} with no handlers registered", typeof(TEvent).FullName);
             }
-            
+
             foreach (var (handler, arg0, arg1, arg2) in _handlers)
             {
                 handler(ev, arg0, arg1, arg2);
@@ -113,15 +148,35 @@ internal class EventQueue(ILogger logger)
             entry = new Entry<TEvent, TArg>(logger);
             _handlersArg1[key] = entry;
         }
-        
+
         ((Entry<TEvent, TArg>)entry).RegisterHandler(handler, arg);
-        
+
         if (!_entriesByEventType.TryGetValue(typeof(TEvent), out var entryList))
         {
             entryList = new List<EntryBase>();
             _entriesByEventType[typeof(TEvent)] = entryList;
         }
-        
+
+        entryList.Add(entry);
+    }
+
+    public void RegisterOpaqueHandler<TArg>(Type eventType, Action<object, TArg> handler, TArg arg)
+    {
+        var key = (eventType, typeof(TArg));
+        if (!_handlersArg1.TryGetValue(key, out var entry))
+        {
+            entry = new OpaqueEntry<TArg>(logger);
+            _handlersArg1[key] = entry;
+        }
+
+        ((OpaqueEntry<TArg>)entry).RegisterHandler(handler, arg);
+
+        if (!_entriesByEventType.TryGetValue(eventType, out var entryList))
+        {
+            entryList = new List<EntryBase>();
+            _entriesByEventType[eventType] = entryList;
+        }
+
         entryList.Add(entry);
     }
 
@@ -133,18 +188,18 @@ internal class EventQueue(ILogger logger)
             entry = new Entry<TArg0, TArg1>(logger);
             _handlersArg2[key] = entry;
         }
-        
+
         ((Entry<TEvent, TArg0, TArg1>)entry).RegisterHandler(handler, arg0, arg1);
-        
+
         if (!_entriesByEventType.TryGetValue(typeof(TEvent), out var entryList))
         {
             entryList = new List<EntryBase>();
             _entriesByEventType[typeof(TEvent)] = entryList;
         }
-        
+
         entryList.Add(entry);
     }
-    
+
     public void RegisterHandler<TEvent, TArg0, TArg1, TArg2>(Action<TEvent, TArg0, TArg1, TArg2> handler, TArg0 arg0, TArg1 arg1, TArg2 arg2)
     {
         var key = (typeof(TEvent), typeof(TArg0), typeof(TArg1), typeof(TArg2));
@@ -153,7 +208,7 @@ internal class EventQueue(ILogger logger)
             entry = new Entry<TArg0, TArg1, TArg2>(logger);
             _handlersArg3[key] = entry;
         }
-        
+
         ((Entry<TEvent, TArg0, TArg1, TArg2>)entry).RegisterHandler(handler, arg0, arg1, arg2);
 
         if (!_entriesByEventType.TryGetValue(typeof(TEvent), out var entryList))
@@ -161,7 +216,7 @@ internal class EventQueue(ILogger logger)
             entryList = new List<EntryBase>();
             _entriesByEventType[typeof(TEvent)] = entryList;
         }
-        
+
         entryList.Add(entry);
     }
 
@@ -172,7 +227,7 @@ internal class EventQueue(ILogger logger)
             var typedHandlers = (Action<TEvent>)handlers;
             typedHandlers?.Invoke(ev);
         }
-        
+
         if (_entriesByEventType.TryGetValue(typeof(TEvent), out var entryList))
         {
             foreach (var entry in entryList)
