fe1b16c869f0b966e5bd4ed4a56a21e70c9a3bb7
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-05-25 14:50:01 +0200

    Use general closure trampolines for event handlers

--- src/ReadyM.Api/Mapping/Events/NativeMappedEventManager.cs
+++ src/ReadyM.Api/Mapping/Events/NativeMappedEventManager.cs
@@ -4,6 +4,7 @@ using Friflo.Engine.ECS;
 using Microsoft.Extensions.Logging;
 using ReadyM.Api.ECS.Registry;
 using ReadyM.Api.Helpers;
+using ReadyM.Api.Interop;
 using ReadyM.Api.Interop.Registry;
 using ReadyM.Api.Mapping.Tags;
 
@@ -28,19 +29,13 @@ internal class NativeMappedEventManager(
         ManagedTarget = GCHandle.ToIntPtr(GCHandle.Alloc(this))
     };
 
-    public void RegisterNativeGameEventHandler(int eventId, NativeEventCallback callback)
+    public void RegisterNativeGameEventHandler(int eventId, ClosureTrampoline1 callback)
     {
         var eventType = nativeRegistry.GetComponentType(eventId);
 
         if (eventType is null || !typeof(IInteropType).IsAssignableFrom(eventType))
         {
             logger.LogError("Attempted to register native GAME event callback for unknown event ID {EventId}", eventId);
-            return;
-        }
-
-        if (!callback.IsValid)
-        {
-            logger.LogError("Attempted to register invalid native GAME event callback for event type {EventType}", eventType.FullName);
             return;
         }
 
@@ -58,19 +53,13 @@ internal class NativeMappedEventManager(
         }, callback);
     }
 
-    public void RegisterNativeEcsEventHandler(int eventId, NativeEventCallback callback)
+    public void RegisterNativeEcsEventHandler(int eventId, ClosureTrampoline1 callback)
     {
         var eventType = nativeRegistry.GetComponentType(eventId);
 
         if (eventType is null || !typeof(IInteropType).IsAssignableFrom(eventType))
         {
             logger.LogError("Attempted to register native ECS event callback for unknown event ID {EventId}", eventId);
-            return;
-        }
-
-        if (!callback.IsValid)
-        {
-            logger.LogError("Attempted to register invalid native ECS event callback for event type {EventType}", eventType.FullName);
             return;
         }
 
@@ -87,7 +76,7 @@ internal class NativeMappedEventManager(
             }
         }, callback);
     }
-    
+
     public bool NotifyEcsIfApplicable(int eventId, IntPtr data)
     {
         var eventType = nativeRegistry.GetComponentType(eventId);
@@ -99,7 +88,7 @@ internal class NativeMappedEventManager(
 
         if (!typeof(IAlwaysPropagates).IsAssignableFrom(eventType) && !typeof(IAlwaysPropagatesToEcsOnly).IsAssignableFrom(eventType))
         {
-            logger.LogError("Attempted to notify ECS of event with id {EventId} and type {EventType} which does not implement IOwnershipManaged", eventId, eventType.FullName);
+            logger.LogError("Attempted to notify ECS of event with id {EventId} and type {EventType} which does not implement IAlwaysPropagates", eventId, eventType.FullName);
             return false;
         }
 
@@ -163,5 +152,94 @@ internal class NativeMappedEventManager(
         }
 
         return true;
+    }
+
+    public bool InvokeInGameIfApplicable(int eventId, IntPtr data)
+    {
+        var eventType = nativeRegistry.GetComponentType(eventId);
+        if (eventType is null)
+        {
+            logger.LogError("Attempted to invoke in Game an unknown event with id {EventId}", eventId);
+            return false;
+        }
+
+        if (!typeof(IAlwaysPropagates).IsAssignableFrom(eventType) && !typeof(IAlwaysPropagatesToEcsOnly).IsAssignableFrom(eventType))
+        {
+            logger.LogError("Attempted to invoke in Game an event with id {EventId} and type {EventType} which does not implement IAlwaysPropagates", eventId, eventType.FullName);
+            return false;
+        }
+
+        if (!policyDir.ForEventOpaque<EmptyContext>(eventType).CanEcsInvokeGameEvent(default))
+            return false;
+
+        using (sideChannel.PushScope(PropagationDirection.ToGame, eventId))
+        {
+            var ev = Marshal.PtrToStructure(data, eventType);
+
+            if (ev is not null)
+            {
+                incomingGameEventQueue.Invoke(ev, eventType);
+            }
+            else
+            {
+                logger.LogError("Failed to marshal native Game event with id {EventId} and type {eventName}", eventId, eventType.FullName);
+            }
+        }
+
+        return true;
+    }
+
+    // TODO: For now, we hard-code IntPtr as context, for IOwnershipManaged events
+    public bool InvokeInGameIfApplicable(int eventId, IntPtr data, IntPtr context)
+    {
+        var eventType = nativeRegistry.GetComponentType(eventId);
+        if (eventType is null)
+        {
+            logger.LogError("Attempted to invoke in Game an unknown event with id {EventId}", eventId);
+            return false;
+        }
+
+        if (!typeof(IOwnershipManaged).IsAssignableFrom(eventType))
+        {
+            logger.LogError("Attempted to invoke in Game an event with id {EventId} and type {EventType} which does not implement IOwnershipManaged", eventId, eventType.FullName);
+            return false;
+        }
+
+        if (!entityMapper.IsMapped(context, out var entity))
+        {
+            logger.LogError("Failed to map entity context {Context} for native Game event with id {EventId} and type {eventType}", context, eventId, eventType.FullName);
+            return false;
+        }
+
+        if (!policyDir.ForEventOpaque<Entity>(eventType).CanEcsInvokeGameEvent(entity.Value))
+            return false;
+
+        using (sideChannel.PushScope(PropagationDirection.ToGame, eventId))
+        {
+            var ev = Marshal.PtrToStructure(data, eventType);
+
+            if (ev is not null)
+            {
+                incomingGameEventQueue.Invoke(ev, eventType);
+            }
+            else
+            {
+                logger.LogError("Failed to marshal native ECS event with id {EventId} and type {eventName}", eventId, eventType.FullName);
+            }
+        }
+
+        return true;
+    }
+
+    public void InvokeInGameAndNotifyEcs(int eventId, IntPtr data)
+    {
+        NotifyEcsIfApplicable(eventId, data);
+        InvokeInGameIfApplicable(eventId, data);
+    }
+
+    public void InvokeInGameAndNotifyEcs(int eventId, IntPtr data, IntPtr context)
+    {
+        NotifyEcsIfApplicable(eventId, data, context);
+        InvokeInGameIfApplicable(eventId, data, context);
     }
 }
