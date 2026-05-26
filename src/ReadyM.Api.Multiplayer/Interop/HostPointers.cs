f85d4e06f91579dfefbc1f1463f8ca018133ea61
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-05-26 14:54:17 +0200

    Pass all host pointers in one struct

---
+++ src/ReadyM.Api.Multiplayer/Interop/HostPointers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ReadyM.Api.Multiplayer.Interop;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct HostPointers
+{
+    public IntPtr GetComponentIdByName;
+    public IntPtr EmbedQuery1;
+    public IntPtr EmbedQuery2;
+}
