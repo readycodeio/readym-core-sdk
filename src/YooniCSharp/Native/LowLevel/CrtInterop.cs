c43668ef732d69d5bed45fe12fd8fac79e489b0f
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-05-30 17:10:37 +0200

    Fix CPP allocator on Linux

--- src/YooniCSharp/Native/LowLevel/CrtInterop.cs
+++ src/YooniCSharp/Native/LowLevel/CrtInterop.cs
@@ -5,21 +5,33 @@ namespace Yooni.Native.LowLevel;
 #if NET8_0_OR_GREATER
 internal static unsafe partial class CrtInterop
 {
-    [LibraryImport("ucrtbase", EntryPoint = "malloc")]
+#if WINDOWS || _WINDOWS
+    private const string CLib = "ucrtbase";
+#else
+    private const string CLib = "libc";
+#endif
+
+    [LibraryImport(CLib, EntryPoint = "malloc")]
     [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
     internal static partial void* Malloc(nuint size);
 
-    [LibraryImport("ucrtbase", EntryPoint = "free")]
+    [LibraryImport(CLib, EntryPoint = "free")]
     [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
     internal static partial void Free(void* ptr);
 }
 #else
 internal static unsafe class CrtInterop
 {
-    [DllImport("ucrtbase", EntryPoint = "malloc", CallingConvention = CallingConvention.Cdecl)]
+#if WINDOWS || _WINDOWS
+    private const string CLib = "ucrtbase";
+#else
+    private const string CLib = "libc";
+#endif
+
+    [DllImport(CLib, EntryPoint = "malloc", CallingConvention = CallingConvention.Cdecl)]
     internal static extern void* Malloc(nuint size);
 
-    [DllImport("ucrtbase", EntryPoint = "free", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport(CLib, EntryPoint = "free", CallingConvention = CallingConvention.Cdecl)]
     internal static extern void Free(void* ptr);
 }
 #endif
