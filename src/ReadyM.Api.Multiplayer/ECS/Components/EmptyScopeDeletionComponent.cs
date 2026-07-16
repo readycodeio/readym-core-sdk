90bcee031846114f6a0e7e092759a32b734b1bd5
Kacper Dudziak <44618240+Moritur@users.noreply.github.com>
2026-07-16 12:24:23 +0200

    Add EmptyScopeDeletionComponent and tests
    This commit only sets up tests, but the implementation will be added later, as part of TDD approach

---
+++ src/ReadyM.Api.Multiplayer/ECS/Components/EmptyScopeDeletionComponent.cs
@@ -0,0 +1,15 @@
+﻿using ReadyM.Api.Attributes;
+using ReadyM.Api.Multiplayer.Generators;
+using System.Runtime.InteropServices;
+
+namespace ReadyM.Api.Multiplayer.ECS.Components;
+
+[DeriveINetworkedComponent(emitDirtyMask: false), NativeComponent]
+[StructLayout(LayoutKind.Sequential)]
+internal partial struct EmptyScopeDeletionComponent
+{
+    private byte _dirtyMask;
+    private byte _apiMask;
+
+    private bool _doNotDeleteWhenAllPlayersLeave;
+}
