0f1a6dfbe34409f6f40ddccf7d3573c2167f8701
Kacper Dudziak <44618240+Moritur@users.noreply.github.com>
2026-07-17 15:15:09 +0200

    Transfer ownership to server when players leave
    Transfer ownership of all remaining entities to the server when all players leave a scope to avoid a situation where a scope is not deleted, so it keeps its entities, but they are owned by a player that is no longer in that scope (the last player to leave)

--- src/ReadyM.Api.Multiplayer/ECS/Tags/DisallowOwnershipTransferOnScopeLeaveTag.cs
+++ src/ReadyM.Api.Multiplayer/ECS/Tags/DisallowOwnershipTransferOnScopeLeaveTag.cs
@@ -2,5 +2,9 @@
 
 namespace ReadyM.Api.Multiplayer.ECS.Tags;
 
-/// <exclude />
+/// <summary>
+/// Entities with this tag are destroyed when their owner leaves their scope.
+/// Other entities get their ownership transferred to another player in that scope or to server
+/// if there are no players left and that scope is not deleted when all players leave.
+/// </summary>
 public readonly struct DisallowOwnershipTransferOnScopeLeaveTag : ITag;
