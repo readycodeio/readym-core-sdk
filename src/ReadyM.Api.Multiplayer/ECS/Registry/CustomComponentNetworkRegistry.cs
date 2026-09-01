30488adb0322f452de03297bd95d0821a5f80b3a
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-09-01 11:55:50 +0200

    Add a way to register mod component for replication client-side

---
+++ src/ReadyM.Api.Multiplayer/ECS/Registry/CustomComponentNetworkRegistry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using LiteNetLib;
+using ReadyM.Api.Multiplayer.ECS.Components;
+
+namespace ReadyM.Api.Multiplayer.ECS.Registry;
+
+internal class CustomComponentNetworkRegistry : INetworkedComponentRegistration, IComponentRegistry
+{
+    private readonly List<Type> _registeredComponentTypes = [];
+
+    public void Register(INetworkedComponentRegistry registry)
+    {
+        foreach (var type in _registeredComponentTypes)
+        {
+            registry.RegisterComponent(type, DeliveryMethod.ReliableOrdered);
+        }
+    }
+
+    public IComponentRegistry RegisterComponent<T>() where T : struct, INetworkedComponent
+    {
+        if (_registeredComponentTypes.Contains(typeof(T)))
+            throw new InvalidOperationException($"Component type {typeof(T).FullName} is already registered.");
+
+        _registeredComponentTypes.Add(typeof(T));
+
+        return this;
+    }
+}
---
+++ src/ReadyM.Api.Multiplayer/ECS/Registry/IComponentRegistry.cs
@@ -0,0 +1,15 @@
+﻿using ReadyM.Api.Multiplayer.ECS.Components;
+
+namespace ReadyM.Api.Multiplayer.ECS.Registry;
+
+/// <summary>
+/// Allows registering components with the ECS system, both local and networked.
+/// </summary>
+public interface IComponentRegistry
+{
+    /// <summary>
+    /// Registers a networked (replicated over the network) component type with the ECS system.
+    /// </summary>
+    /// <typeparam name="T">The type of the component to register. Must be a struct that implements <see cref="INetworkedComponent"/>.</typeparam>
+    IComponentRegistry RegisterComponent<T>() where T : struct, INetworkedComponent;
+}
