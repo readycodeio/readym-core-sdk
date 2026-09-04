9f9f34ff031ec858d1d7a55194c9bbd3f3511556
waxx <prog112@gmail.com>
2026-09-04 14:28:49 +0200

    Added rotating cell masters

--- src/ReadyM.Relay.Server.Sdk/Interop/PlayerApiPointers.cs
+++ src/ReadyM.Relay.Server.Sdk/Interop/PlayerApiPointers.cs
@@ -10,4 +10,5 @@ public struct PlayerApiPointers
     public required IntPtr RemovePlayerEventHandler;
     public required IntPtr KickPlayer;
     public required IntPtr GetReadyMId;
+    public required IntPtr RotateCellMasters;
 }
