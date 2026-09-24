b200c0601e8fe8f8ea65810075b41df0d2782ea3
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-09-24 18:01:57 +0200

    Add IVT for Oblivion

--- src/ReadyM.SDK/AssemblyInfo.cs
+++ src/ReadyM.SDK/AssemblyInfo.cs
@@ -6,6 +6,7 @@
 [assembly: InternalsVisibleTo("ReadyM.SDK.Benchmarks")]
 [assembly: InternalsVisibleTo("WukongMp.Sdk")]
 [assembly: InternalsVisibleTo("WukongMp.Sdk.Common")]
+[assembly: InternalsVisibleTo("OblivionMp.Sdk.Common")]
 [assembly: InternalsVisibleTo("ReadyM.Relay.Server.PluginHost")]
 [assembly: InternalsVisibleTo("ReadyM.Api.Generators.Tests.Dynamic")]
 
